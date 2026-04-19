9bdb4ad0d60604832cf3a5d3966d899b52b3e800
Anshika Moundekar <moundekaranshika2@gmail.com>
2026-04-19 16:38:23 +0530

    Update model.py

--- scripts/model.py
+++ scripts/model.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 def train_model():
     df = pd.read_csv("data/processed_data.csv")
 
-    # Label: risky driving
     df["label"] = (
         df["speeding"] |
         df["harsh_braking"] |
@@ -12,13 +13,17 @@ def train_model():
         df["sharp_turn"]
     )
 
-    X = df[["speed", "acceleration", "gyro"]]
+    X = df[["speed", "acceleration", "gyro", "speed_avg", "acc_avg"]]
     y = df["label"]
 
-    model = RandomForestClassifier()
-    model.fit(X, y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    print("Model trained successfully!")
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+
+    print(classification_report(y_test, preds))
 
 if __name__ == "__main__":
     train_model()
