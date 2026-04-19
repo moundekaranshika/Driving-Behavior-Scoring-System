9c1d770a94a1ebf8263d15f469dd0e852811fdff
Anshika Moundekar <moundekaranshika2@gmail.com>
2026-04-19 16:38:08 +0530

    Update scoring.py

--- scripts/scoring.py
+++ scripts/scoring.py
@@ -3,16 +3,18 @@ import pandas as pd
 def calculate_score():
     df = pd.read_csv("data/processed_data.csv")
 
+    total = len(df)
+
     penalties = (
-        df["speeding"].sum() * 2 +
-        df["harsh_braking"].sum() * 3 +
-        df["rapid_acceleration"].sum() * 2 +
-        df["sharp_turn"].sum() * 2
+        (df["speeding"].sum() / total) * 30 +
+        (df["harsh_braking"].sum() / total) * 30 +
+        (df["rapid_acceleration"].sum() / total) * 20 +
+        (df["sharp_turn"].sum() / total) * 20
     )
 
     score = max(0, 100 - penalties)
 
-    print(f"Driving Score: {score}")
+    print(f"Driving Score: {score:.2f}")
 
     return score
 
